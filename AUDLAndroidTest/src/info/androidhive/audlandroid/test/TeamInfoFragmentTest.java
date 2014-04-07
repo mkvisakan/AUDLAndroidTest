fdf117ef5f36437539b26ad8fb0488324c0ad3c6
mkvisakan <mails4visak@gmail.com>
2014-04-07 12:26:56 -0500

    Fixed the format of parseJson to support testing

--- AUDLAndroidTest/src/info/androidhive/audlandroid/test/TeamInfoFragmentTest.java
+++ AUDLAndroidTest/src/info/androidhive/audlandroid/test/TeamInfoFragmentTest.java
@@ -50,16 +50,17 @@ public class TeamInfoFragmentTest extends android.test.ActivityInstrumentationTe
     }
 	
 	public void testParseJson() throws Exception {
-		String response = "[[[\"Madison\", \"Radicals\", 224002], [\"Benjy K\", \"1\"], [\"Andrew M\", \"2\"]], [\"Madison Radicals\", 224002, [\"4/12/14\", \"7:30 PM EST\", \"Cincinnati Revolution\"], [\"4/13/14\", \"3:30 PM EST\", \"Indianapolis AlleyCats\"], [\"4/19/14\", \"12:00 AM EST\", \"Detroit Mechanix\"]], [[\"Madison\", \"Radicals\", 224002], [\"Goals\", [[\"Scott R\", 42], [\"Nate T\", 36], [\"Pat S\", 36]]], [\"Assists\", [[\"Animal\", 55]]], [\"Drops\", [[\"Dave W\", 8]]], [\"Throwaways\", [[\"Dave W\", 56]]], [\"PMC\", [[\"Scott R\", 75], [\"Pat S\", 61]]], [\"Ds\", [[\"Dave W\", 28]]]]]";
+		String response = "[[[\"Madison\", \"Radicals\", 224002], [\"Benjy K\", \"1\"], [\"Andrew M\", \"2\"]], [\"Madison Radicals\", 224002, [\"4/12/14\", \"7:30 PM EST\", \"Cincinnati Revolution\", 183001], [\"4/13/14\", \"3:30 PM EST\", \"Indianapolis AlleyCats\", 253001], [\"4/19/14\", \"12:00 AM EST\", \"Detroit Mechanix\", 219001]], [[\"Madison\", \"Radicals\", 224002], [\"Goals\", [[\"Scott R\", 42], [\"Nate T\", 36], [\"Pat S\", 36]]], [\"Assists\", [[\"Animal\", 55]]], [\"Drops\", [[\"Dave W\", 8]]], [\"Throwaways\", [[\"Dave W\", 56]]], [\"PMC\", [[\"Scott R\", 75], [\"Pat S\", 61]]], [\"Ds\", [[\"Dave W\", 28]]]]]";
 		JSONArray jsonResult = new JSONArray(response);
-		teamFrag.parseJSON(jsonResult); 
-		TeamsListItem team = teamFrag.getTeam(); 
+		TeamsListItem team = teamFrag.parseJSON(jsonResult, "Madison Radicals", "224002"); 
 		assertEquals("Testing the parse json of TeamsInfoFragment", 2, team.getPlayerNames().size());
 		assertEquals("Testing the parsing of team player names", "Benjy K", team.getPlayerNames().get(0));
 		assertEquals("Testing the parsing of team player id", "1", team.getPlayerIds().get(0));
-		assertEquals("Testing the parsing of team schedule", 3, team.getSchedDateTimes().size());
+		assertEquals("Testing the parsing of team schedule", 3, team.getSchedDates().size());
 		assertEquals("Testing the parsing of schedule team name", "Cincinnati Revolution", team.getSchedTeams().get(0));
-		assertEquals("Testing the parsing of schedule team name", "4/12/14 7:30 PM EST", team.getSchedDateTimes().get(0));
+		assertEquals("Testing the parsing of schedule team name", "183001", team.getSchedTeamIds().get(0));
+		assertEquals("Testing the parsing of schedule date", "4/12/14", team.getSchedDates().get(0));
+		assertEquals("Testing the parsing of schedule time", "7:30 PM EST", team.getSchedTimes().get(0));
 		assertEquals("Testing the parsing of team goal stats", 3, team.getGoalPlayers().size());
 		assertEquals("Testing the parsing of team goal player", "Scott R", team.getGoalPlayers().get(0));
 		assertEquals("Testing the parsing of team goals", "42", team.getGoals().get(0));
