d025af4a5faefb884ad7e6601fc74132f362d1ff
Nirav Gandhi <ngandhi1993@gmail.com>
2014-04-09 03:02:47 -0500

    Added Test cases for Scores and Standings

---
+++ AUDLAndroidTest/src/info/androidhive/audlandroid/test/ScoresListFragmentTest.java
@@ -0,0 +1,48 @@
+package info.androidhive.audlandroid.test;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import info.androidhive.audlandroid.MainActivity;
+import info.androidhive.audlandroid.ScoresListFragment;
+import info.androidhive.audlandroid.model.ScoreListItem;
+
+public class ScoresListFragmentTest extends android.test.ActivityInstrumentationTestCase2<MainActivity>{
+	
+	private FragmentManager mManager;
+	private ScoresListFragment mFragment;
+	private MainActivity activity;
+	
+	public ScoresListFragmentTest() {
+		super(MainActivity.class);
+	}
+	
+	protected void setUp() throws Exception{
+		super.setUp();
+		setActivityInitialTouchMode(false);
+		activity = getActivity();
+		
+		mFragment = new ScoresListFragment();
+		mManager = activity.getSupportFragmentManager();
+		mManager.beginTransaction().add(android.R.id.content, mFragment,"tag").commit();
+		getInstrumentation().waitForIdleSync();
+		
+		mFragment = (ScoresListFragment) mManager.findFragmentByTag("tag");
+	}
+	public void testAvailability() throws Exception{
+		assertNotNull(mFragment);
+	}
+	
+	public void testParseJson() throws Exception{
+		String response = "[[\"Western\", [[\"San Francisco FlameThrowers\", 5094953273262080, \"San Jose Spiders\", 6597766625099776, \"4/12/14\", \"7:00 PM PST\", \"0-0\"], [\"Vancouver Riptide\", 6226234774126592, \"Salt Lake Lions\", 5716606839685120, \"4/12/14\", \"7:00 PM MST\", \"0-0\"]]]]";
+		JSONArray jsonResult = new JSONArray(response);
+		ArrayList<ArrayList<ScoreListItem>> scoreList = mFragment.parseJSON(jsonResult);
+		Log.i("Test","Nirav" + scoreList.toString());
+		assertEquals("Number of divisions",1,scoreList.size());
+		assertEquals("Number of Scores in Western division",scoreList.get(0).size(),2);
+		assertTrue(scoreList.get(0).get(0).equals(new ScoreListItem("San Francisco FlameThrowers","5094953273262080","San Jose Spiders","6597766625099776","4/12/14","7:00 PM PST","0-0")));
+	}
+}
---
+++ AUDLAndroidTest/src/info/androidhive/audlandroid/test/StandingsListFragmentTest.java
@@ -0,0 +1,49 @@
+package info.androidhive.audlandroid.test;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import info.androidhive.audlandroid.MainActivity;
+import info.androidhive.audlandroid.StandingsListFragment;
+import info.androidhive.audlandroid.model.TeamRecordItem;
+import android.support.v4.app.FragmentManager;
+
+public class StandingsListFragmentTest extends android.test.ActivityInstrumentationTestCase2<MainActivity>{
+	private FragmentManager mFragmentManager;
+	private StandingsListFragment mFragment;
+	private MainActivity activity;
+	public StandingsListFragmentTest() {
+		super(MainActivity.class);
+	}
+	
+	protected void setUp() throws Exception{
+		super.setUp();
+		setActivityInitialTouchMode(false);
+		activity = getActivity();
+		
+		mFragment = new StandingsListFragment();
+		mFragmentManager = activity.getSupportFragmentManager();
+		mFragmentManager.beginTransaction().add(android.R.id.content, mFragment,"tag").commit();
+		getInstrumentation().waitForIdleSync();
+		mFragment = (StandingsListFragment) mFragmentManager.findFragmentByTag("tag");
+	}
+	
+	public void testAvailability() throws Exception {
+		assertNotNull(mFragment);
+	}
+	
+	public void testParseJson() throws Exception {
+		String response = "[[\"Midwest Division\", [\"Madison Radicals\", 10, 3], [\"Chicago Wildfire\", 9, 4]], [\"Eastern Division\", [\"DC Breeze\", 10, 3], [\"New York Empire\", 9, 4]]]";
+		JSONArray jsonResult = new JSONArray(response);
+		ArrayList<ArrayList<TeamRecordItem>> leagueRecords = mFragment.parseJSON(jsonResult);
+		assertEquals("Number of Divisions",2,leagueRecords.size());
+		assertEquals("Number of Teams in Midwest Division",2,leagueRecords.get(0).size());
+		assertEquals("Number of Teams in Eastern Division",2,leagueRecords.get(0).size());
+		assertTrue("First recordItem in Midwest Division",leagueRecords.get(0).get(0).equals(new TeamRecordItem("Madison Radicals","10","3")));
+		assertTrue("Second recordItem in Midwest Division",leagueRecords.get(0).get(1).equals(new TeamRecordItem("Chicago Wildfire","9","4")));
+		assertTrue("First recordItem in Eastern Division",leagueRecords.get(1).get(0).equals(new TeamRecordItem("DC Breeze","10","3")));
+		assertTrue("Second recordItem in Eastern Division",leagueRecords.get(1).get(1).equals(new TeamRecordItem("New York Empire","9","4")));
+	}
+}
